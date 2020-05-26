974addec4d243e869e0fda27552125ece38b00a9
ani <anulikapple@gmail.com>
2020-05-26 17:24:58 +0100

    makeGrid can work with any number of user input columns/rows

--- battleships/battleships.py
+++ battleships/battleships.py
@@ -1,12 +1,12 @@
 from pprint import pprint
 
 
-def makeGrid(cols):
+def makeGrid(rows, cols):
     grid = []
-    for x in range(0, cols):
+    for x in range(0, rows):
         row = []
         grid.append(row)
-        for y in range(0, 10):
+        for y in range(0, cols):
             row.append(0)
 
     return grid
@@ -19,7 +19,7 @@ def shoot(row, col):
 
 
 
-grid2 = makeGrid(10)
+grid2 = makeGrid(10, 10)
 pprint(grid2)
 
 
@@ -29,3 +29,5 @@ pprint(grid2)
 #make grid with 10 cols + 10 rows - DONE
 #each cell has 0 - DONE
 #shoot func --> accepts col + row index and replaces 0 with 1
+
+#create a function that prints grids neatly
