9bbe4e01bb71851de75d5e190ae56d7789b6c716
artemb <a.bobrovskij@gmail.com>
2020-07-13 19:02:37 +0100

    Keyboard controls and physics

--- test/drawing.py
+++ test/drawing.py
@@ -7,8 +7,12 @@ SCREEN_WIDTH = 1250
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "zombie game"
 
-TILE_SCALE = 0.25
-TILE_SIZE = 32
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 15
+
+TILE_SCALE = 0.5
+TILE_SIZE = 64
 
 BOX_NO = 10
 
@@ -34,17 +38,20 @@ class MyGame(arcade.Window):
         self.ground = None
         self.boxes = None
 
+        self.physics_engine = None
+
     def setup(self):
         # Create your sprites and sprite lists here
         self.setup_zombie()
         self.setup_ground()
         self.setup_boxes()
 
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.zombie, self.ground, GRAVITY)
+
     def setup_zombie(self):
         self.zombie = arcade.Sprite(":resources:images/animated_characters/zombie/zombie_idle.png", scale=TILE_SCALE)
         self.zombie.center_x = TILE_SIZE / 2
         self.zombie.center_y = TILE_SIZE + TILE_SIZE / 2
-        self.zombie.draw()
 
     def setup_ground(self):
         self.ground = arcade.SpriteList()
@@ -53,7 +60,6 @@ class MyGame(arcade.Window):
                 lava = arcade.Sprite(":resources:images/tiles/lava.png", scale=TILE_SCALE, center_x=TILE_SIZE / 2, center_y=TILE_SIZE / 2)
                 lava.center_x += i*TILE_SIZE
                 self.ground.append(lava)
-        self.ground.draw()
 
     def setup_boxes(self):
         self.boxes = arcade.SpriteList()
@@ -62,7 +68,6 @@ class MyGame(arcade.Window):
                 box = arcade.Sprite(":resources:images/tiles/boxCrate.png", scale=TILE_SCALE, center_x=TILE_SIZE * 2.5, center_y=TILE_SIZE * 1.5)
                 box.center_x += i*(TILE_SIZE+(BOX_NO*7))
                 self.boxes.append(box)
-        self.boxes.draw()
 
 
 
@@ -74,12 +79,12 @@ class MyGame(arcade.Window):
         # This command should happen before we start drawing. It will clear
         # the screen to the background color, and erase what we drew last frame.
         arcade.start_render()
-        # self.zombie.draw()
-        # self.ground.draw()
-        # self.boxes.draw()
-        self.setup_zombie()
-        self.setup_ground()
-        self.setup_boxes()
+        self.zombie.draw()
+        self.ground.draw()
+        self.boxes.draw()
+        # self.setup_zombie()
+        # self.setup_ground()
+        # self.setup_boxes()
 
 
 
@@ -90,8 +95,32 @@ class MyGame(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
+        self.physics_engine.update()
 
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
 
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.zombie.change_y = PLAYER_JUMP_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.zombie.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.zombie.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.zombie.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        # if key == arcade.key.UP or key == arcade.key.W:
+        #     self.zombie.change_y = 0
+        # if key == arcade.key.DOWN or key == arcade.key.S:
+        #     self.zombie.change_y = 0
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.zombie.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.zombie.change_x = 0
 
 
 def main():
